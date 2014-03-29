cb958b156fdcaa8d70314ff413ad48076ed0c55d
Kirk North <kirk.w.north@gmail.com>
2014-03-29 16:23:52 -0400

    MOD: retrieve setup.py
    Adding the f2py extensions to the setup. As of now this requires the pyf
    interface files for each of the corresponding Fortran routines.

--- pyart/retrieve/setup.py
+++ pyart/retrieve/setup.py
@@ -1,9 +1,29 @@
-
+"""
+"""
 
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('retrieve', parent_package, top_path)
     #config.add_data_dir('tests')
+    config.add_extension('background',
+                         sources=['background.pyf','src/background.f90'],
+                         f2py_options=[])
+    config.add_extension('continuity',
+                         sources=['continuity.pyf','src/continuity.f90'],
+                         f2py_options=[])
+    config.add_extension('divergence',
+                         sources=['divergence.pyf','src/divergence.f90'],
+                         f2py_options=[])
+    config.add_extension('gradient',
+                         sources=['gradient.pyf','src/gradient.f90'],
+                         f2py_options=[])
+    config.add_extension('laplace',
+                         sources=['laplace.pyf','src/laplace.f90'],
+                         f2py_options=[])
+    config.add_extension('smooth',
+                         sources=['smooth.pyf','src/smooth.f90'],
+                         f2py_options=[])
+    
     return config
 
 
