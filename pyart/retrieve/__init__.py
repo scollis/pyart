b2e93edbcfec2d9c045ba2cfc1aeed222df04e20
Scott Collis <scollis.acrf@gmail.com>
2014-11-12 19:16:05 -0600

    FIX:Added advection to pyart.retrieve.__init__.py

--- pyart/retrieve/__init__.py
+++ pyart/retrieve/__init__.py
@@ -20,6 +20,11 @@ try:
 except:
     _F90_EXTENSIONS_AVAILABLE = False
 
-from .texture import texture
+try:
+    from .advection import  grid_displacememt_pc
+    _ADVECTION_AVAILABLE = True
+except:
+    _ADVECTION_AVAILABLE = False
+
 
 __all__ = [s for s in dir() if not s.startswith('_')]
