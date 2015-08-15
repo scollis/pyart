a2e7d31b8ad56df419e0d20b06ddcc485a5d6549
scollis <scollis.acrf@gmail.com>
2015-08-14 20:34:52 -0400

    WIP: Added velocity texture code

--- pyart/retrieve/circular_texture.py
+++ pyart/retrieve/circular_texture.py
@@ -1,5 +1,5 @@
 """
-pyart.correct.circular_textur
+pyart.correct.circular_texture
 =========================
 Application of a circular texture algorithm that works on data that
 folds between an interval to produce a texture field for future
@@ -16,7 +16,7 @@ from ..config import get_metadata
 from ..util import interval_std
 from scipy.ndimage import filters
 
-def generic_circ_filter(radar, field, interval,
+def generic_circ_texture(radar, field, interval,
         texture_footprint = (4,4), median_footprint = None):
     """
     Use the standard deviation of a moment that is valid (ie folds)
@@ -52,6 +52,39 @@ def generic_circ_filter(radar, field, interval,
     texture_field['standard_name'] = 'texture_of_' +\
             texture_field['standard_name']
     return texture_field
+
+
+
+def velocity_circ_texture(radar, field, interval,
+        texture_footprint = (4,4), median_footprint = None,
+        velocity_key ='velocity' ):
+    """
+    Use the standard deviation of velocity to calculate a texture field
+    Assumes nyquist_velocity is in the instrument parameters
+    ----------
+    radar : Radar
+        Radar object containing field to calculate the texture fron
+    texture_footprint: twople
+        footprint of the standard deviation calculation in azimuth index and
+        range index. Defaults to (4,4)
+    median_footprint: twople
+        footprint of the median filter to be applied after
+        in azimuth index and range index. Defaults to None which means no
+        median filter applied.
+    Returns
+    -------
+    texture_field : dict
+        Field dictionary containing texure information. Texture
+        array is stored under the 'data' key and nyquist does not vary
+        through the volume
+    """
+    nyq = radar.instrument_parameters['nyquist_velocity']['data'][0]
+    return generic_circ_texture(radar, velocity_key,
+            interval = [-nyq, nyq], texture_footprint = texture_footprint,
+            median_footprint = median_footprint)
+
+
+
 
 
 
